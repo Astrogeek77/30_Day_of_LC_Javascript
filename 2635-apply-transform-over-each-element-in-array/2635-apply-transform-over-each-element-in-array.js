5452662ee3e3bbb450c2f1333e33e64f019ec12a
Gautam Jain <astrogeek77@outlook.com>
2023-05-10 10:05:09 +0530

    Added Day 4 Problem -> #2635

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    var newArr = [];
+    
+    for(var i = 0; i < arr.length; i++)
+    {
+        newArr[i] = fn(arr[i], i);
+    }
+    
+    return newArr;
+};
